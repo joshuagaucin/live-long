2dc2a02cc0cbfa2e09d3586407eb78babfd3f432
Joshua Gaucin <joshuagaucin01@gmail.com>
2020-12-16 20:39:35 -0600

    Updates

---
+++ precache-manifest.bf2ef4bfbff6db8310bfc4d39658b23e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cce72075c2a6b200e992a0cb7dfb27",
+    "url": "/live-long/index.html"
+  },
+  {
+    "revision": "f3e52adf846939f5f27a",
+    "url": "/live-long/static/css/2.55140da4.chunk.css"
+  },
+  {
+    "revision": "c641b0e24c4f920c0757",
+    "url": "/live-long/static/css/main.e2e5ed8d.chunk.css"
+  },
+  {
+    "revision": "f3e52adf846939f5f27a",
+    "url": "/live-long/static/js/2.0911f795.chunk.js"
+  },
+  {
+    "revision": "de881aea9a299eab2174439464268bee",
+    "url": "/live-long/static/js/2.0911f795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c641b0e24c4f920c0757",
+    "url": "/live-long/static/js/main.209c7cbb.chunk.js"
+  },
+  {
+    "revision": "e4a399b5d7052f388958",
+    "url": "/live-long/static/js/runtime-main.327a54d7.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/live-long/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/live-long/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/live-long/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/live-long/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/live-long/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/live-long/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ec40e5e87839fa04f4055893bae2537a",
+    "url": "/live-long/static/media/food-hero.ec40e5e8.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/live-long/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/live-long/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/live-long/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/live-long/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/live-long/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/live-long/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/live-long/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/live-long/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/live-long/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/live-long/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "6686f206face5cdc8e52621623218f29",
+    "url": "/live-long/static/media/site-hero.6686f206.jpg"
+  },
+  {
+    "revision": "94b1454e4a81776fb62c1fa31774c15c",
+    "url": "/live-long/static/media/training-hero.94b1454e.jpg"
+  }
+]);
